c783a11e0a901c48fb60a1209d69adcafb04f1fd
charlottel91 <charlotte.leconte91@gmail.com>
2021-06-01 09:32:58 +0200

    Checked if user exist in bdd.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,29 @@
 const UserService = require('../services/UserService');
 
 module.exports = class UserController {
+  static async apiCheckedIfUserExist(req, res) {
+    try {
+      const user = req.body.params;
+      const checkedIfUsersExist = await UserService.checkedIfUserExist(user);
+      if(checkedIfUsersExist) return res.status(404).json({message: 'already exist'});
+    } catch(error) {
+      res.status(500).json({error: error});
+    }
+  }
+
   static async apiCreateUser(req, res) {
     try {
       const value = req.body;
-      const createdUsers = await value.map(user => UserService.createUser(user));
-      res.json (createdUsers);
+      let userChecked = await UserService.checkedIfUserExist(value);
+      if (userChecked) {
+        return res.status(409).json({
+          msg: `${value.firstname.charAt(0).toUpperCase() + value.firstname.slice(1)} ${value.lastname.toUpperCase()} est déjà enregistré.e`,
+          userChecked
+        });
+      } else {
+        UserService.createUser(value);
+      }
+      res.json (userChecked);
     } catch(error) {
       res.status(500).json({error: error});
     }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -4,14 +4,23 @@ module.exports = class UserService {
   static async createUser(value) {
     try {
       const newUser = {
-        firstname: value.firstname,
-        lastname: value.lastname,
+        firstname: value.firstname.toLowerCase(),
+        lastname: value.lastname.toLowerCase(),
         isChild: value.child,
         isVegetarian: value.vegetarian,
         presentBrunch: value.brunch,
       };
       const response = await new User(newUser).save();
       return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async checkedIfUserExist(user) {
+    try {
+      let checkedUser = await User.findOne({ firstname: user.firstname, lastname: user.lastname });
+      return checkedUser;
     } catch (error) {
       console.log(error);
     }
