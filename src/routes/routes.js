ebacf37a47cc0ca100f93717b655e565dc4aa7ac
charlottel91 <charlotte.leconte91@gmail.com>
2021-07-16 23:49:40 +0200

    Modify routes.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const AuthController = require('../controllers/auth.controller');
-const CarpoolingController = require('../controllers/carpooling.controller');
 const UserController = require('../controllers/user.controller');
 
 const router = express.Router();
 
-router.post('/api/signup', AuthController.signup);
-router.post('/api/signin', AuthController.signin);
-router.get('/user/:id', UserController.apiGetOneUser);
-router.post('/user/:id/carpooling', CarpoolingController.apiCreateCarpoolingInUser);
-router.put('/register/:id', UserController.apiCreateGuestInUser);
-router.put('/user/:id/delete/guest', UserController.apiDeleteGuestInUser);
+router.get('user/:id', UserController.apiGetOneUser);
+router.post('api/signup', AuthController.signup);
+router.post('api/signin', AuthController.signin);
+router.put('register/:id', UserController.apiCreateGuestInUser);
+router.put('user/:id/delete/guest', UserController.apiDeleteGuestInUser);
 
 module.exports = router;
