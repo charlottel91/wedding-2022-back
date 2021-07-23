facf3c76f0c1cf2e527c60cb437864dde18e8b8f
charlottel91 <charlotte.leconte91@gmail.com>
2021-07-23 19:47:23 +0200

    Add and modify carpooling.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,9 +4,10 @@ const UserController = require('../controllers/user.controller');
 
 const router = express.Router();
 
-router.get('/user/:id', UserController.apiGetOneUser);
 router.post('/api/signup', AuthController.signup);
 router.post('/api/signin', AuthController.signin);
+router.get('/user/:id', UserController.apiGetOneUser);
+router.post('/user/:id/carpooling', UserController.apiCreateCarpoolingInUser);
 router.put('/register/:id', UserController.apiCreateGuestInUser);
 router.put('/user/:id/delete/guest', UserController.apiDeleteGuestInUser);
 
