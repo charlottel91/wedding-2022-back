c0cb86a201f95bd064bf65a5ec7241e6e7f0c000
charlottel91 <charlotte.leconte91@gmail.com>
2021-09-26 11:58:41 +0200

    Delete carpooling part.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const AuthController = require('../controllers/auth.controller');
-const CarpoolingController = require('../controllers/carpooling.controller');
 const UserController = require('../controllers/user.controller');
 
 const router = express.Router();
@@ -8,7 +7,6 @@ const router = express.Router();
 router.post('/api/signup', AuthController.signup);
 router.post('/api/signin', AuthController.signin);
 router.get('/user/:id', UserController.apiGetOneUser);
-router.post('/user/:id/carpooling', CarpoolingController.apiCreateCarpoolingInUser);
 router.put('/register/:id', UserController.apiCreateGuestInUser);
 router.put('/user/:id/delete/guest', UserController.apiDeleteGuestInUser);
 
